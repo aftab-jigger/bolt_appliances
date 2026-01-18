ef68d50544974b9cb804e16afd2a33c62bffee72
Aftab <aftab@Aftabs-MacBook-Pro.local>
2026-01-18 12:25:13 +0000

    Added Product Categories Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/layout/Navbar/Navbar";
 import AboutSection from "./components/sections/AboutSection";
 import Hero from "./components/sections/Hero";
 import StatisticsSection from "./components/sections/StatisticsSection";
+import ProductCategories from "./components/sections/ProductCategories";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <AboutSection />
       <StatisticsSection />
+      <ProductCategories />
       <main className="mt-16">
         <section id="products" className="py-16">
           <h3 className="text-3xl text-center font-bold">Our Products</h3>
